06ad124350593158500165dc0ccff8f7408eb9f2
Chris Hill-Scott <me@quis.cc>
2016-01-06 15:41:42 +0000

    Sort out folder structure, add init

--- build_tools/packager/jinja_packager.rb
+++ build_tools/packager/jinja_packager.rb
@@ -11,12 +11,16 @@ module Packager
     end
 
     def build
-      @target_dir = @repo_root.join('pkg', @base_name)
+      @target_dir = @repo_root.join('pkg', @base_name, 'govuk_template_jinja')
       @target_dir.rmtree if @target_dir.exist?
       @target_dir.mkpath
       Dir.chdir(@target_dir) do |dir|
-        generate_setup_py
         prepare_contents
+        add_package_init
+      end
+      @target_dir = @repo_root.join('pkg', @base_name)
+      Dir.chdir(@target_dir) do |dir|
+        generate_setup_py
         create_tarball
       end
     end
@@ -30,11 +34,25 @@ module Packager
       end
     end
 
+    def add_package_init
+      output, status = Open3.capture2e("touch #{File.join(@target_dir, "__init__.py")}")
+      abort "Error creating __init__.py:\n#{output}" if status.exitstatus > 0
+    end
+
     def process_template(file)
       target_dir = @target_dir.join(File.dirname(file))
       target_dir.mkpath
       File.open(target_dir.join(generated_file_name(file)), 'wb') do |f|
         f.write Compiler::JinjaProcessor.new(file).process
+      end
+    end
+
+    def create_tarball
+      Dir.chdir(@target_dir.join('..')) do
+        @repo_root.join("pkg").mkpath
+        target_file = @repo_root.join("pkg", "#{@base_name}.tgz").to_s
+        output, status = Open3.capture2e("tar -czf #{target_file.shellescape} -C #{@base_name.shellescape} .")
+        abort "Error creating tar:\n#{output}" if status.exitstatus > 0
       end
     end
 
